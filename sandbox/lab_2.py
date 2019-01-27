37178d73bd95316b25eae402c8f460b2e13d426b
Max Bergmark <max.bergmark@gmail.com>
2019-01-27 12:09:08 +0100

    Added scripts for lab 3

--- sandbox/lab_2.py
+++ sandbox/lab_2.py
@@ -1,121 +1,120 @@
 import numpy as np
 
 def normalize(v):
-	return v / np.linalg.norm(v)
+    return v / np.linalg.norm(v)
 
 def norm(v):
-	return np.linalg.norm(v)
+    return np.linalg.norm(v)
 
 # def gram_schmidt(A):
-	# Q = np.zeros_like(A, dtype = np.float64)
-	# Q[:,0] = normalize(A[:,0])
-	# for j in range(1, A.shape[1]):
-		# col = A[:,j].copy()
-		# for i in range(j):
-			# col -= np.dot(A[:,j], Q[:,i]) * Q[:,i]
-		# Q[:,j] = normalize(col)
-	# return Q
+    # Q = np.zeros_like(A, dtype = np.float64)
+    # Q[:,0] = normalize(A[:,0])
+    # for j in range(1, A.shape[1]):
+        # col = A[:,j].copy()
+        # for i in range(j):
+            # col -= np.dot(A[:,j], Q[:,i]) * Q[:,i]
+        # Q[:,j] = normalize(col)
+    # return Q
 
-def g_s(A):
-	n = A.shape[0]
-	V = A.copy()
-	Q = np.zeros_like(A, dtype = np.float64)
-	R = Q.copy()
-	for i in range(A.shape[0]):
-		r_ii = norm(V[:,i])
-		R[i,i] = r_ii
-		q_i = V[:,i] / r_ii
-		for j in range(i+1, n):
-			r_ij = np.dot(q_i, V[:,j])
-			V[:,j] -= r_ij * q_i
-			R[i,j] = r_ij
-		Q[:,i] = q_i
-	return Q, R
+def factorize_qr(A):
+    n = A.shape[0]
+    V = A.copy()
+    Q = np.zeros_like(A, dtype = np.float64)
+    R = Q.copy()
+    for i in range(A.shape[0]):
+        r_ii = norm(V[:,i])
+        R[i,i] = r_ii
+        q_i = V[:,i] / r_ii
+        for j in range(i+1, n):
+            r_ij = np.dot(q_i, V[:,j])
+            V[:,j] -= r_ij * q_i
+            R[i,j] = r_ij
+        Q[:,i] = q_i
+    return Q, R
 
 def solve_QR(A, b):
-	n = A.shape[0]
-	Q, R = g_s(A)
-	b_q = np.dot(Q.T, b)
-	x = np.zeros_like(b)
-	for j in range(n-1, -1, -1):
-		x_sum = np.dot(R[j, j+1:], x[j+1:])
-		x[j] = (b_q[j] - x_sum) / R[j,j]
-
-	return x
+    n = A.shape[0]
+    Q, R = factorize_qr(A)
+    b_q = np.dot(Q.T, b)
+    x = np.zeros_like(b)
+    for j in range(n-1, -1, -1):
+        x_sum = np.dot(R[j, j+1:], x[j+1:])
+        x[j] = (b_q[j] - x_sum) / R[j,j]
+    return x
 
 def solve_least_squares(A, b):
-	new_A = np.dot(A.T, A)
-	new_b = np.dot(A.T, b)
-	return solve_QR(new_A, new_b)
+    new_A = np.dot(A.T, A)
+    new_b = np.dot(A.T, b)
+    return solve_QR(new_A, new_b)
 
 def is_diagonal(A):
-	return np.allclose(A, np.diag(np.diag(A)))
+    return np.allclose(A, np.diag(np.diag(A)))
 
-def is_upper_diagonal(A):
-	n = A.shape[0]
-	for row in range(n):
-		if not np.isclose(A[row,:row].sum(), 0, atol = 1e-15):
-			return False
-	return True
+def is_upper_triangular(A):
+    n = A.shape[0]
+    for row in range(n):
+        if not np.isclose(A[row,:row].sum(), 0, atol = 1e-15):
+            return False
+    return True
 
 def get_eigenvalues(A):
-	A_c = A.copy()
-	n = A.shape[0]
-	pQ = np.eye(n)
-	while not is_upper_diagonal(A_c):
-	# while not is_diagonal(A_c):
-		Q, R = g_s(A_c)
-		pQ = np.dot(pQ, Q)
-		A_c = np.dot(R, Q)
-	eigs = np.diag(A_c)
-	return eigs, pQ
+    A_c = A.copy()
+    n = A.shape[0]
+    pQ = np.eye(n)
+    while not is_upper_triangular(A_c):
+    # while not is_diagonal(A_c):
+        Q, R = factorize_qr(A_c)
+        pQ = np.dot(pQ, Q)
+        A_c = np.dot(R, Q)
+    eigs = np.diag(A_c)
+    return eigs, pQ
 
 
 def test_QR_decomposition():
-	n = 5
-	A = np.random.rand(n, n)
-	Q, R = g_s(A)
-	# check that R is upper triangular
-	for row in range(n):
-		assert np.isclose(R[row,:row].sum(), 0)
-	# assert that Q is orthogonal
-	assert np.allclose(np.dot(Q.T, Q).sum(axis = 1), 1)
-	# assert that A = Q*R
-	assert np.allclose(A, np.dot(Q, R))
-	# assert that Q * Q.T is the identity matrix
-	assert np.allclose(np.eye(n), np.dot(Q, Q.T))
+    n = 5
+    A = np.random.rand(n, n)
+    Q, R = factorize_qr(A)
+    # check that R is upper triangular
+    for row in range(n):
+        assert np.isclose(R[row,:row].sum(), 0)
+    # assert that Q is orthogonal
+    assert np.allclose(np.dot(Q.T, Q).sum(axis = 1), 1)
+    # assert that A = Q*R
+    assert np.allclose(A, np.dot(Q, R))
+    # assert that Q * Q.T is the identity matrix
+    assert np.allclose(np.eye(n), np.dot(Q, Q.T))
 
 def test_QR_solve():
-	A = np.array([[1, 2], [3, 4]], dtype = np.float64)
-	x = np.array([5, 6])
-	b = np.dot(A, x)
-	x_test = solve_QR(A, b)
-	assert np.allclose(x, x_test)
-	# print("true_value:", np.dot(A, x))
-	# print("test_x:", x_test)
-	# print("test_value:", np.dot(A, x_test))
-	
+    A = np.array([[1, 2], [3, 4]], dtype = np.float64)
+    x = np.array([5, 6])
+    b = np.dot(A, x)
+    x_test = solve_QR(A, b)
+    assert np.allclose(x, x_test)
+    # print("true_value:", np.dot(A, x))
+    # print("test_x:", x_test)
+    # print("test_value:", np.dot(A, x_test))
+    
 def test_least_squares():
-	A = np.array([[1, -1], [1, 1], [2, 1]], dtype = np.float64)
-	b = np.array([2, 4, 8])
-	x_true = np.array([23/7, 8/7])
-	x_test = solve_least_squares(A, b)
-	assert np.allclose(x_true, x_test)
+    A = np.array([[1, -1], [1, 1], [2, 1]], dtype = np.float64)
+    b = np.array([2, 4, 8])
+    x_true = np.array([23/7, 8/7])
+    x_test = solve_least_squares(A, b)
+    assert np.allclose(x_true, x_test)
 
 def test_eigenvalues(A):
-	eig_val, eig_vec = get_eigenvalues(A)
-	n = A.shape[0]
-	for i in range(n):
-		v0 = eig_val[i] * eig_vec[:,i]
-		v1 = np.dot(A, eig_vec[:,i])
-		assert np.allclose(v0, v1)
+    eig_val, eig_vec = get_eigenvalues(A)
+    n = A.shape[0]
+    for i in range(n):
+        v0 = eig_val[i] * eig_vec[:,i]
+        v1 = np.dot(A, eig_vec[:,i])
+        assert np.allclose(v0, v1)
 
 def test_all_eigen():
-	test_eigenvalues(np.array([[0, -1], [-1, -3]], dtype = np.float64))
-	# test_eigenvalues(np.array([[1, 2, 0], [-2, 1, 3], [0, -3, 1]], dtype = np.float64))
-	for n in range(2, 9):
-		A = np.random.rand(n, n)
-		test_eigenvalues(A + A.T)
+    test_eigenvalues(np.array([[0, -1], [-1, -3]], dtype = np.float64))
+    # test_eigenvalues(np.array([[1, 2, 0], [-2, 1, 3], [0, -3, 1]], dtype = np.float64))
+    for n in range(2, 9):
+        A = np.random.rand(n, n)
+        test_eigenvalues(A + A.T)
 
 
 test_QR_decomposition()
---
+++ sandbox/lab_3.py
@@ -0,0 +1,152 @@
+import numpy as np
+
+def normalize(v):
+    return v / np.linalg.norm(v)
+
+def norm(v):
+    return np.linalg.norm(v)
+
+def jacobi_iterate(A, b):
+    D = np.diag(np.diag(A))
+    R = A - D
+    D_inv = np.diag(1/np.diag(D))
+    x = np.ones(A.shape[1])
+    for _ in range(1000):
+        x = np.dot(D_inv, b - np.dot(R, x))
+    return x
+
+def gauss_seidel(A, b):
+    x = np.zeros(A.shape[1])
+    for _ in range(1000):
+        x_new = x.copy()
+        for i in range(x.size):
+            L_sum = np.dot(A[i,:i], x_new[:i])
+            U_sum = np.dot(A[i,i+1:], x[i+1:])
+            x_new[i] = 1/A[i,i]*(b[i] - L_sum - U_sum)
+            x = x_new
+    return x_new
+
+def df(f, x, h):
+    return (f(x+h) - f(x-h)) / (2*h)
+
+def newton(f, x):
+    iters = 0
+    fx = f(x)
+    while np.max(abs(fx)) > 1e-15:
+        fx = f(x)
+        x -= fx / df(f, x, 1e-8)
+        iters += 1
+        if iters > 100000:
+            print(f(x))
+            raise ValueError("No solution found")
+    return x
+
+def arnoldi(A, b, k, Q, H, k_max):
+    v = np.dot(A, Q[:,k])
+    for j in range(k):
+        H[j,k] = np.dot(Q[:,j], v)
+        v -= H[j,k]*Q[:,j]
+    H[k+1,k] = norm(v)
+    if (H[k+1, k] != 0 and k != k_max -1):
+        Q[:,k+1] = v / H[k+1,k]
+
+def gmres(A, b):
+    k_max = 100
+    n = A.shape[0]
+    x0 = np.zeros(n)
+
+    x = np.zeros(n)
+    r = b - np.dot(A, x0)
+
+    Q = np.zeros((n, k_max))
+    H = np.zeros((k_max+1, k_max))
+    Q[:,0] = normalize(r)
+
+    for k in range(k_max):
+        arnoldi(A, b, k, Q, H, k_max)
+
+        b = np.zeros(k_max+1)
+        b[0] = norm(r)
+
+        res = np.linalg.lstsq(H, b, rcond = None)[0]
+        x = np.dot(Q, res) + x0
+
+    return x
+
+
+def test_jacobi_iteration():
+    A = np.array([[2, 1], [5, 7]], dtype = np.float64)
+    b = np.array([11, 13])
+    x_true = np.array([7+1/9, -3-2/9])
+    x = jacobi_iterate(A, b)
+    assert np.allclose(x, x_true)
+    A = np.array(
+        [
+            [10, -1, 2, 0], 
+            [-1, 11, -1, 3], 
+            [2, -1, 10, -1], 
+            [0, 3, -1, 8]
+        ], dtype = np.float64
+    )
+    b = np.array([6, 25, -11, 15])
+    x_true = np.array([1, 2, -1, 1])
+    x = jacobi_iterate(A, b)
+    assert np.allclose(x, x_true)
+
+def test_gauss_seidel():
+    A = np.array([[16, 3], [7, -11]], dtype = np.float64)
+    b = np.array([11, 13])
+    x_true = np.array([0.81218274, -0.66497462])
+    x = gauss_seidel(A, b)
+    assert np.allclose(x, x_true)
+    A = np.array([[2, 1], [5, 7]], dtype = np.float64)
+    b = np.array([11, 13])
+    x_true = np.array([7+1/9, -3-2/9])
+    x = gauss_seidel(A, b)
+    assert np.allclose(x, x_true)
+
+def test_polynomial():
+    for _ in range(100):
+        p = np.random.randn()
+        q = np.random.randn()
+        f = lambda x: x**2 + p*x + q
+        if q < p*p/4:
+            x_0 = -p/2 + (p*p/4 - q)**.5
+            x_1 = -p/2 - (p*p/4 - q)**.5
+            x_start = 0
+            # print(x_0, x_1)
+            x = newton(f, x_start)
+            assert np.isclose(x, x_0) or np.isclose(x, x_1)
+            # print(x, x_0, x_1)
+
+def test_GMRES():
+    A = np.array([[2, 1], [5, 7]], dtype = np.float64)
+    b = np.array([11, 13])
+    x_true = np.array([7+1/9, -3-2/9])
+    # x = GMRes(A, b, np.zeros(A.shape[1]), 0, 5)
+    # print(x)
+    x = gmres(A, b)
+    print(x)
+    print(x_true)
+    assert np.allclose(x, x_true)
+    
+
+def test_vector_polynomial():
+    f = lambda x: (x - 1)**2 
+    g = lambda x: (x- np.arange(x.size))**3
+    for n in range(1, 10):
+        x = np.zeros(n)
+        x = newton(f, x)
+        assert np.allclose(x, 1)
+        x = np.zeros(n)
+        x = newton(g, x)        
+        assert np.allclose(x, np.arange(x.size))
+
+
+
+test_jacobi_iteration()
+test_gauss_seidel()
+test_polynomial()
+test_GMRES()
+test_vector_polynomial()
+print("All tests passed!")
